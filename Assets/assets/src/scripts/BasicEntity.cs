f931c7b6380c2b33d3c2ade9b4e1f8fd9fe59153
Pensathor <73364625+ranal-star@users.noreply.github.com>
2026-01-27 20:44:34 -0300

    Implemnets basic animation for player

--- Assets/assets/src/scripts/BasicEntity.cs
+++ Assets/assets/src/scripts/BasicEntity.cs
@@ -1,26 +1,36 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class BasicEntity : MonoBehaviour
 {
     public string ID_ENTITY {get; set;}
 
-    public virtual void SetAnimationWalking(string state, Animator animator)
-    {
-        int IDHash = Animator.StringToHash(state);
-        
-        Transform GetEntity = gameObject.transform;
+    protected Dictionary<string, int> HashAnimations = new Dictionary<string, int>();
 
-        if(GetEntity.position != Vector3.zero)
+    protected virtual void Awake()
+    {
+
+        RegisterAnimation("OnMoving");
+        RegisterAnimation("OnIdle");
+    }
+
+    protected void RegisterAnimation(string name)
+    {
+        if (!HashAnimations.ContainsKey(name))
         {
-            animator.SetBool(IDHash, true);
-        }
-        else
-        {
-            animator.SetBool(IDHash, false);
+            HashAnimations.Add(name, Animator.StringToHash(name));
         }
     }
 
-    public virtual void SetAnimationIdle(string state, Animator animator)
+    protected virtual void SetAnimationWalking(Animator animator, bool IsMoving)
+    {
+        if(HashAnimations.TryGetValue("OnMoving", out int hash))
+        {
+            animator.SetBool(hash, IsMoving);
+        }
+    }
+
+    protected virtual void SetAnimationIdle(string state, Animator animator)
     {
         //implemment idle animation soon
     }
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # The-language-hunter-Game
+
+The game is set in a world where humanity has been fragmented by the confusion of languages after Nimrod's attempt to build a tower that would reach the heavens. The narrative focus lies on the group “The Linguists,” who possess the knowledge of divine languages capable of shaping reality, facing the curse left by the tyrant before his death.
